ab2d1ebb39783fefdccd704451e3ddaae5f9d6a6
Prabhat Kumar <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-08-17 19:27:18 +0000

    Login and SignUp Page Setup

---
+++ src/pages/login/Login.js
@@ -0,0 +1,23 @@
+import './login.css'
+
+const Login = () => {
+    return (
+        <div className='loginPage' >
+            <div className="loginContainer">
+                <div className="loginConatainerLeft">
+                    <p className='leftLogo'>SocailBee</p>
+                    <p className='leftLogoMessage'>Welcome to Social Bee</p>
+                </div>
+                <div className="loginContainerRight">
+                    <div className="loginCard">
+                        <input type="button" value="" placeholder='Hiiii' className='loginInput'/>
+                        <input type="button" value="" placeholder='Hiiii' className='loginInput' />
+                        <button> hiii</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
