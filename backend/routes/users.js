a1c1c87c21955444400300ef28085697a5ca689c
Prabhat Kumar <108947576+PrabhatKumardhiman@users.noreply.github.com>
2023-08-23 23:21:58 +0000

    Login EndPoint Created

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,11 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+
+dotenv.config()// receving files form .env
 
 router.get("/", (req, res) => {
   res.send("Hello from router user");
@@ -28,7 +33,7 @@ router.post(
       // Generating a secured password
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
-      
+
       // check if user already exists 
       const user = await User.findOne({ email });
       if (user) {
@@ -37,12 +42,12 @@ router.post(
 
       // Creating a new user with secured password
       const newUser = await new User({
-        name : name,
-        username : username,
-        email : email,
-        password : hashedPassword,
+        name: name,
+        username: username,
+        email: email,
+        password: hashedPassword,
       });
-      
+
       // saving the new user with hashed password in database
       await newUser.save() && res.status(200).send(newUser);
     } catch (error) {
@@ -50,5 +55,48 @@ router.post(
     }
   }
 );
+
+// Login user
+router.post(
+  "/login",
+  [
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password can not be blank").notEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    //condition that checks if there are any error. which return the error
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { email, password } = req.body;
+
+      // check if user does not exists 
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).send("User Does Not Exist");
+      }
+
+      // Decrypting password from password
+      const comparedPassword = await bcrypt.compare(password, user.password)
+      if (!comparedPassword) {
+        return res.status(400).send("Wrong Credentails, Please Login Again")
+      }
+      // If user is logged in sharing a jwtToken with the user
+      const data = {
+        user: {
+          id: user.id
+        }
+      }
+      const jwtToken = jwt.sign(data, process.env.JWT_PVT_KEY)
+      res.json({ jwtToken })
+      
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+);
+
 
 module.exports = router;
